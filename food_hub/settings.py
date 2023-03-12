7cd29f9eb3eb75ff4705f70258e0be8a033f2505
raissatito <tito.safaraz@gmail.com>
2023-03-12 16:19:31 +0700

    Add allowed hosts to all

--- food_hub/settings.py
+++ food_hub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5yb(58dol=a5o5i+%jqf=2ecg33*p#-n_z19zm00(2=8(ma60g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
